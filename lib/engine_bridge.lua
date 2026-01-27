2066aee16bc697325d2d396910e8027cee68510f
Josue Arias <31876967+synthi@users.noreply.github.com>
2026-01-27 11:10:39 -0300

    Update engine_bridge.lua

--- lib/engine_bridge.lua
+++ lib/engine_bridge.lua
@@ -1,7 +1,8 @@
--- code/ltra/lib/engine_bridge.lua | v0.7
+-- code/ltra/lib/engine_bridge.lua | v0.9.5
 local Bridge = {}
 local Globals
 local Loopers = require 'ltra/lib/loopers'
+local Consts = require 'ltra/lib/consts'
 
 function Bridge.init(g_ref) Globals = g_ref end
 
@@ -20,15 +21,31 @@ function Bridge.handle_osc(path, args)
     end
 end
 
+-- Sincronización Masiva (Post-Load)
+function Bridge.sync_matrix()
+    for s_name, s_idx in pairs(Consts.SOURCES) do
+        for d_name, d_idx in pairs(Consts.DESTINATIONS) do
+            local val = Globals.matrix[s_idx][d_idx]
+            if val > 0 then
+                -- Reconstruir ID param
+                local idx = string.match(d_name, "(%d+)$") or ""
+                local dest = d_name:lower():gsub("%d", "")
+                if dest == "delay_t" then dest = "delay_time" end
+                if dest == "delay_f" then dest = "delay_fb" end
+                if dest == "filt" then dest = "filt" end 
+                
+                local param_id = "mod_" .. s_name:lower() .. "_" .. dest .. idx
+                engine.param(param_id, val)
+            end
+        end
+    end
+end
+
 function Bridge.query_config() engine.query_config() end
 function Bridge.set_param(name, value) engine.param(name, value) end
 function Bridge.set_freq(idx, hz) engine.param("freq"..idx, hz) end
 function Bridge.set_gate(idx, val) engine.param("gate"..idx, val) end
-
--- NUEVO: Trigger para Arp
 function Bridge.trigger_arp(idx) engine.param("t_arp"..idx, 1) end
-
--- NUEVO: Reset LFOs
 function Bridge.reset_lfo() engine.param("t_reset", 1) end
 
 function Bridge.set_filter_tone(idx, val)
